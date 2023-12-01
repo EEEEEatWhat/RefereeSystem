63e9dd290dd1970d1d4a2b8ab3cd4a6e81050ec7
suzukisuncy <chengyangsun79@gmail.com>
2023-12-01 15:17:07 +0800

    添加Serial部分 | 校验流程

--- src/referee_system/include/referee_system/DataType.h
+++ src/referee_system/include/referee_system/DataType.h
@@ -4,13 +4,17 @@
 #include <iostream>
 namespace RM_referee{
     // Packet header structure
+    #pragma pack(1)
     struct PacketHeader {
         uint8_t SOF;
         uint16_t DataLength;
         uint8_t SequenceNumber;
         uint8_t CRC8;
     } ;
-    // static_assert(sizeof(PacketHeader) == 5, "PacketHeader must be 5 bytes long with packing");
+    #pragma pack()
+    static_assert(sizeof(PacketHeader) == 5, "PacketHeader must be 5 bytes long with packing");
+    
+    static constexpr uint8_t StartOfFrame = 0xa5;
 
     // Base packet
     class RefereePacket {
@@ -22,9 +26,6 @@ namespace RM_referee{
 
     class GameStatusPacket : public RefereePacket {
     public:
-        virtual void SolvePacket(uint8_t*,uint8_t*) override {
-                
-
-        };
+        virtual void SolvePacket(uint8_t*,uint8_t*) override ;
     };
 }
--- src/referee_system/include/referee_system/MappingTables.h
+++ src/referee_system/include/referee_system/MappingTables.h
@@ -6,7 +6,7 @@
         @brief 通过映射表来解析数据
         @param 映射表接受cmd_id，对应DJI裁判系统附录
         @param 解析包函数，通过继承数据包基类并重写Solve函数提供解包方法
-        @date 2023/11/27 基于DJI裁判系统协议V1.5'
+        @date  2023/11/27 基于DJI裁判系统协议V1.5'
         @return NULL
     */
 namespace RM_referee {
@@ -26,7 +26,12 @@ namespace RM_referee {
 
     TypeMethodsTables::TypeMethodsTables(){}
     TypeMethodsTables::~TypeMethodsTables() {}
-
+    /**
+     * @brief  向工厂添加ID和绑定的类
+     * @param  Type 类型名
+     * @param  cmd_id 
+     * @return NONE
+    */
     template <typename Type>
     void TypeMethodsTables::AddTypeMethod(uint16_t cmd_id) {
         m_map.emplace(cmd_id, std::make_shared<Type>());
