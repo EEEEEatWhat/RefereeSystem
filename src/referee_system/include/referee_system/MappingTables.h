5dbe7661251355bd6a63d3bd236a587e57969d1d
suzukisuncy <chengyangsun79@gmail.com>
2023-12-04 23:29:10 +0800

    添加测试函数&测试解析数据

--- src/referee_system/include/referee_system/MappingTables.h
+++ src/referee_system/include/referee_system/MappingTables.h
@@ -1,34 +1,49 @@
+/**
+ * @author suzukisuncy
+ * @date 2023/11/27
+ * @brief 通过映射表来解析数据,映射表接受cmd_id，对应DJI裁判系统附录,基于DJI裁判系统协议V1.6
+*/
+#ifndef MAPPING_TABLES_H
+#define MAPPING_TABLES_H
 #pragma once 
 #include "DataType.h"
 #include <memory>
 #include <unordered_map>
+#include <map>
 
-    /**
-        @brief 通过映射表来解析数据
-        @param 映射表接受cmd_id，对应DJI裁判系统附录
-        @param 解析包函数，通过继承数据包基类并重写Solve函数提供解包方法
-        @date  2023/11/27 基于DJI裁判系统协议V1.5'
-        @return NULL
-    */
 namespace RM_referee {
     using std::hex;
 
     class TypeMethodsTables {
     protected:
-        std::unordered_map< uint16_t, std::shared_ptr<GameStatusPacket>> m_map;
+        std::map< uint16_t, std::shared_ptr<RefereePacket>> m_map;
     public:
         TypeMethodsTables();
         ~TypeMethodsTables();
+        void testprocess();
 
-        template <typename Type>
-        void AddTypeMethod(uint16_t);
+    /**
+     * @brief  向工厂添加ID和绑定的类
+     * @param  Type 类型名
+     * @param  cmd_id 
+     * @return NONE
+    */
+    template <typename Type>
+    void AddTypeMethod(uint16_t cmd_id) {
+        m_map.emplace(cmd_id, std::make_shared<Type>());
+    }
+
         void SerialReadAsync(boost::asio::serial_port& ,std::vector<uint8_t>& );
     protected:
+        /**
+         * @brief 注意检验cmd_id
+        */
         void MapSolve(const uint16_t, uint8_t *, uint8_t *);
-    };
 
+    };
     
 
 }  // namespace RM_referee
 
 
+#endif // MAPPING_TABLES_H
--- src/referee_system/src/test.cpp
+++ src/referee_system/src/test.cpp
@@ -1,25 +1,30 @@
+/**
+ * @author suzukisuncy
+ * @date 2023/11/27
+ * @brief 通过映射表来解析数据,映射表接受cmd_id，对应DJI裁判系统附录,基于DJI裁判系统协议V1.6
+*/
+
 #include <iostream>
 #include <vector>
 #include <boost/asio.hpp>
 #include "DataType.h"
 #include "MappingTables.h"
-
-
-
-
-int main(int argc, char const *argv[])
+using std::hex;
+int main()
 {
+    /*
     boost::asio::io_service ioService;
     boost::asio::serial_port serialPort(ioService, "/dev/ttyUSB0");
     std::vector<uint8_t> buffer(4096);  // 适当调整缓冲区大小
+    */
     RM_referee::TypeMethodsTables Factory_;
     Factory_.AddTypeMethod<RM_referee::GameStatusPacket>(0x0001);
-    Factory_.SerialReadAsync(serialPort,buffer);
+    Factory_.AddTypeMethod<RM_referee::ExtSupplyProjectileAction>(0x0102);
+    Factory_.AddTypeMethod<RM_referee::PowerHeatDataPacket>(0x0202);
+    Factory_.AddTypeMethod<RM_referee::CustomRobotDataPacket>(0x0302);
+    while(1) {
+        Factory_.testprocess();
+    };
     return 0;
 }
-
-
-
-
-
 
