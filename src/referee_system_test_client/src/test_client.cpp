32f6233ff3db8ccbdf0984260999703f340bdfe6
suzukisuncy <chengyangsun79@gmail.com>
2024-03-02 17:56:27 +0800

    [ADD]客户端测试
    TestPassed

--- src/referee_system_test_client/src/test_client.cpp
+++ src/referee_system_test_client/src/test_client.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "rclcpp/rclcpp.hpp"
 #include "my_msg_interface/srv/referee_msg.hpp"
+#include "DataType.h"
 class RefereeSystem_Test_Client : public rclcpp::Node {
     public:
         RefereeSystem_Test_Client(): Node("RefereeSystemTestClient") {
@@ -41,19 +42,33 @@ int main(int argc, char *argv[])
     while (i++) {
         uint16_t cmd_id = 0x102;
         cmd_id += (i%3)*0x100;
+        cmd_id  =0x202;
         auto response = client->send_request(cmd_id);
         RCLCPP_INFO(client->get_logger(),"发送请求:0x%x",cmd_id);
         // 处理响应
         if (rclcpp::spin_until_future_complete(client,response) == rclcpp::FutureReturnCode::SUCCESS) {
             RCLCPP_INFO(client->get_logger(),"请求正常处理");
-            if(response.get()->data_length != 0) {
-                RCLCPP_INFO(client->get_logger(),"响应正常");
-                auto response_data = reinterpret_cast<void *>(response.get()->data_stream.data());            } 
-
+            auto result = response.get();
+            if(result->data_stream.size() != 0) {
+                RCLCPP_INFO(client->get_logger(),"响应正常11111");
+                RCLCPP_INFO(client->get_logger(),"cmd_id:0x%x,data_length:%d",result->cmd_id,result->data_stream.size());
+                if(result->cmd_id == 0x202) {
+                    RM_referee::PowerHeatDataStruct T;
+                    std::memcpy(&T,result->data_stream.data(),result->data_length);
+                    RCLCPP_INFO(client->get_logger(),"chassis电压(mV):%d",T.chassis_voltage);
+                    RCLCPP_INFO(client->get_logger(),"chassis电流(mA):%d",T.chassis_current);
+                    RCLCPP_INFO(client->get_logger(),"底盘功率(W):%d",T.chassis_power);
+                    RCLCPP_INFO(client->get_logger(),"缓冲能量(J):%d",T.buffer_energy);
+                    RCLCPP_INFO(client->get_logger(),"第1个17mm枪口热量:%d",T.shooter_17mm_1_barrel_heat);
+                    RCLCPP_INFO(client->get_logger(),"第2个17mm枪口热量:%d",T.shooter_17mm_2_barrel_heat);
+                    RCLCPP_INFO(client->get_logger(),"42mm枪口热量:%d",T.shooter_42mm_barrel_heat);
+                }
+                
+            } 
         } else {
             RCLCPP_INFO(client->get_logger(),"请求异常");
         }
-        sleep(1);
+        sleep(2);
     }
     rclcpp::shutdown();
     return 0;
