80448798f737e3bcb15ec9161096fb0afd4341fd
suzukisuncy <chengyangsun79@gmail.com>
2024-07-05 16:04:12 +0800

    [feat]编译新功能
    test passed

---
+++ src/referee_system/launch/replay.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription ,actions
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.actions import ExecuteProcess,RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    referee_system_dir = get_package_share_directory('referee_system')
+    referee_system_yaml_path = os.path.join(referee_system_dir,'launch','param.yaml')
+    config_path = LaunchConfiguration('config_path')
+    declare_config_path_cmd = DeclareLaunchArgument(
+        'config_path', default_value=referee_system_yaml_path,
+        description='Yaml config file path'
+    )
+    t1 =Node(
+        package="referee_system",
+        executable="replay",
+        parameters=[config_path],
+        )
+    return LaunchDescription([
+        declare_config_path_cmd,
+        t1,
+    ])
